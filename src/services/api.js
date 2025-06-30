14838844471207d90bb00a1098707112ffefd900
saiganesh-sristla <saiganesh3108@gmail.com>
2025-06-30 13:41:13 +0530

    changed the backend api endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://crm-backend-lwxu.onrender.com/api' });
 
 export default API;
